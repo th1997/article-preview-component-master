f08ef6b28ed0ad349d6d3e3f71b8f5fed3f3f065
s0kyl <teo59460@gmail.com>
2023-12-29 14:24:14 +0100

    Adding JS script + button animation

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleShare() {
+    var shareBtn = document.getElementById('share_btn');
+    var svgShare = document.getElementById('svg_share');
+  
+    // Ajoute ou supprime la classe 'active' pour changer les styles
+    shareBtn.classList.toggle('active');
+    
+    // Change la couleur de fond en noir
+    if (shareBtn.classList.contains('active')) {
+      shareBtn.style.backgroundColor = 'var(--Very-Dark-Grayish-Blue)';
+
+      // Change la couleur de l'image SVG en blanc
+      svgShare.style.fill = 'white';
+    } else {
+      // Rétablit les styles par défaut si la classe 'active' est supprimée
+      shareBtn.style.backgroundColor = 'var(--Light-Grayish-Blue)';
+      svgShare.style.fill = 'var(--Desaturated-Dark-Blue)';
+    }
+}
